7327700afdb974f1a042efdc6b8ec270ef477d34
Mamanov Emil <emil.mamanov97@gmail.com>
2023-11-03 23:13:25 +0600

    added setups, functions, buttons, html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+
+
+import meatImage from './assets/meat.png';
+import cheeseImage from './assets/cheese.png';
+import saladImage from './assets/salad.png';
+import baconImage from './assets/bacon.png';
+
+const ingredientsData = [
+    { name: 'Meat', price: 80, image: meatImage },
+    { name: 'Cheese', price: 50, image: cheeseImage },
+    { name: 'Salad', price: 10, image: saladImage },
+    { name: 'Bacon', price: 60, image: baconImage },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [ingredients, setIngredients] = useState(
+        ingredientsData.map((ingredient) => ({ ...ingredient, count: 0 }))
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleAddIngredient = (name: string) => {
+        setIngredients((prevIngredients) =>
+            prevIngredients.map((ingredient) =>
+                ingredient.name === name ? { ...ingredient, count: ingredient.count + 1 } : { ...ingredient }
+            )
+        );
+    };
+
+    const handleRemoveIngredient = (name: string) => {
+        setIngredients((prevIngredients) =>
+            prevIngredients.map((ingredient) =>
+                ingredient.name === name && ingredient.count > 0
+                    ? { ...ingredient, count: ingredient.count - 1 } : { ...ingredient }
+            )
+        );
+    };
+
+    const calculateTotalPrice = () => {
+        return ingredients.reduce((total, ingredient) => total + ingredient.price * ingredient.count, 30);
+    };
+
+    const resetIngredients = () => {
+        setIngredients(ingredientsData.map((ingredient) => ({ ...ingredient, count: 0 })));
+    };
+
+    return (
+        <div className="App">
+            <div className="Ingredients">
+                {ingredients.map((ingredient) => (
+                    <Ingredient
+                        key={ingredient.name}
+                        name={ingredient.name}
+                        image={ingredient.image}
+                        count={ingredient.count}
+                        onAdd={() => handleAddIngredient(ingredient.name)}
+                        onRemove={() => handleRemoveIngredient(ingredient.name)}
+                    />
+                ))}
+                <button onClick={resetIngredients}>Сбросить всё</button>
+            </div>
+            <Burger
+                ingredients={ingredients}
+                totalPrice={calculateTotalPrice()}
+                onAddIngredient={handleAddIngredient}
+                onRemoveIngredient={handleRemoveIngredient}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
